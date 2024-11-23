07305ae8ab7b9613242bcee7bd57882c41db6f0c
SatoshiWakasugi <waka0628s@gmail.com>
2024-11-23 11:38:27 +0900

    set up express

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import flavorRouter from "./routes/flavor";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  console.log('"hello express');
+  res.send("<h1>こんにちは</h1>");
+});
+
+// routing
+app.use("/flavor", flavorRouter);
+
+app.listen(PORT, () => console.log("server start"));
